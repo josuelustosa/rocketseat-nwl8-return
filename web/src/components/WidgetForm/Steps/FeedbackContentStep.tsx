6ecc379125ce2493a92025831bcd6ac115610eb2
Josue <josuelustosa.job@gmail.com>
2022-06-05 21:18:03 -0400

    :construction: adding title when selecting feedback type

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,26 @@
-export function FeedbackContentStep() {
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton} from "../../CloseButton";
+
+interface FeedbackTypeStepProps {
+    feedbackType: FeedbackType;
+}
+
+export function FeedbackContentStep({ feedbackType }: FeedbackTypeStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+
     return(
-        
+        <>
+            <header>
+                <span className="text-xl leading-6">
+                    {feedbackTypeInfo.title}
+                </span>
+
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                
+            </div>
+        </>
     )
 }
